ca2f03aff79da73105608eed59e3a30359b578a1
Fabiana Alencar <fabianabyte@gmail.com>
2023-02-19 00:00:37 -0300

    estudos kotlin com springboot

---
+++ src/main/kotlin/com/mercadolivro/basico/TesteInicial.kt
@@ -0,0 +1,21 @@
+package com.mercadolivro.basico
+
+class TesteInicial {
+
+
+}
+
+class Pessoa(var nome : String, var idade : Int){
+    override fun toString() : String{
+        return "Nome: ${nome}, idade: ${idade}"
+    }
+}
+
+data class PessoaData(var nome:String, var idade:Int){
+
+}
+
+fun main(){
+    println(Pessoa("teste", 12).toString())
+    println(PessoaData("teste", 12))
+}
